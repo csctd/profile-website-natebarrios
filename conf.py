453916d3185eb1ea7474ee2d6d9477159f397964
natebarrios <141265943+natebarrios@users.noreply.github.com>
2023-08-04 08:58:09 -0400

    Update conf.py

--- conf.py
+++ conf.py
@@ -1,8 +1,8 @@
 # -- Project information -----------------------------------------------------
 
-project = 'Site Title'
-copyright = '2022, Your name'
-author = 'Your Name'
+project = 'Nate's site'
+copyright = '2023, Nate Barrios'
+author = 'Nate Barrios'
 
 
 
